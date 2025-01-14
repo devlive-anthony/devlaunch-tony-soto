ccc60f4637d5f18a3777a16c76e20e6b9fbd7015
Anthony Soto <anthony@devlive.io>
2025-01-14 14:11:19 -0600

    feat(inheritance): add folder structure

---
+++ object-oriented-programming/inheritance/vehicle.ts
@@ -0,0 +1,15 @@
+/*
+
+Eres responsable de diseñar un sistema sencillo de gestión de vehículos para una empresa de transporte.
+
+El sistema debe representar diferentes tipos de vehículos y sus comportamientos utilizando una estructura de herencia de clases.
+
+Todos los vehículos tienen un emoji, marca, modelo y año de fabricación.
+
+Los vehículos deben poder arrancar y mostrar su información.
+
+Los coches tienen un número específico de puertas y deben poder encender el aire acondicionado.
+
+Las motocicletas pueden tener un sidecar y deben poder hacer una acrobacia en una rueda (wheelie).
+
+*/
