bf457e30bd3f050dc2b09d271080494adc4f99d0
Anthony Soto <anthony@devlive.io>
2025-01-14 14:12:27 -0600

    chore(inheritance): add animal exercise

--- object-oriented-programming/inheritance/animal.ts
+++ object-oriented-programming/inheritance/animal.ts
@@ -17,3 +17,74 @@ y propiedades particulares.
 
 */
 
+class Animal {
+  constructor(
+    protected name: string,
+    protected species: string,
+    protected age: number
+  ) {
+    this.name = name
+    this.species = species
+    this.age = age
+  }
+
+  makeSound() {
+    return ''
+  }
+
+  toString() {
+    return `Name: ${this.name}\nSpicy: ${this.species}\nAge: ${this.age}`
+  }
+}
+
+class Lion extends Animal {
+
+  constructor(
+    name: string,
+    species: string,
+    age: number
+  ) {
+    super(name, species, age)
+  }
+
+  makeSound(): string {
+    return 'Roar...'
+  }
+
+  print(): string {
+    const info = this.toString()
+
+    return `🦁:\n${info}`
+  }
+}
+
+type ElephantSize = 'small' | 'medium' | 'large'
+
+class Elephant extends Animal {
+  constructor(
+    name: string,
+    species: string,
+    age: number,
+    private size: ElephantSize
+  ) {
+    super(name, species, age)
+  }
+
+  makeSound(): string {
+    return 'Phhrr...'
+  }
+
+  print(): string {
+    const info = this.toString()
+
+    return `🐘:\n${info}\nSize: ${this.size}`
+  }
+}
+
+const l1 = new Lion('Simba', 'King', 8)
+console.log(l1.makeSound())
+console.log(l1.print())
+
+const el1 = new Elephant('Dumbo', 'Fly', 1, 'small')
+console.log(el1.makeSound())
+console.log(el1.print())
